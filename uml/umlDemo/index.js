a61b58ab1d5e1866fc562e5256c18eb7ec09754f
leomYili <leomy@outlook.com>
2018-01-18 18:28:18 +0800

    小型demo,group,及各种事件监听

---
+++ uml/umlDemo/index.js
@@ -0,0 +1,91 @@
+$(function () {
+
+    // XX业务代码
+
+    jsPlumb.ready(function () {
+        var instance = window.jsp = jsPlumb.getInstance({
+            DragOptions: { cursor: "pointer", zIndex: 15 },
+            Container: $(".artboard").get(0),
+            Endpoint: "Dot", // 端点类型
+            EndpointStyle: { stroke: "#833", fill: "#F2F2F2", radius: 2.5, strokeWidth: 1 },
+            ConnectionOverlays: [
+                ["Arrow", {
+                    location: 1,
+                    visible: true,
+                    width: 11,
+                    length: 11,
+                    id: "ARROW",
+                    events: {
+                        click: function () { alert("you clicked on the arrow overlay") }
+                    }
+                }],
+                ["Label", {
+                    location: 0.1,
+                    id: "label",
+                    cssClass: "aLabel",
+                    events: {
+                        tap: function () { alert("hey"); }
+                    }
+                }]
+            ]
+        });
+
+        instance.bind("click", function (conn, originalEvent) {
+            // if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
+            //   instance.detach(conn);
+            console.log("click");
+        });
+
+        instance.bind("connectionDrag", function (connection) {
+            console.log("connection " + connection.id + " is being dragged. suspendedElement is ")
+        });
+
+        instance.bind("connectionDragStop", function (connection) {
+            console.log("connection " + connection.id + " was dragged");
+        });
+
+        instance.bind("connectionMoved", function (params) {
+            console.log("connection " + params.connection.id + " was moved");
+        });
+
+        // 编组
+        function addGroup(element, id) {
+            instance.addGroup({
+                el: element,
+                id: id,
+                draggable: true,
+                constrain: true,
+                dragOptions: {
+                    stop: function (e) {
+                        console.log(e);
+                    }
+                }
+            });
+        }
+
+        // 给需要连线的element增加节点
+        function makeEndpoint(el) {
+            // 可以进行连线的起点
+            instance.makeSource(el, {
+                anchor: 'Continuous',
+                maxConnections: -1,
+                allowLoopback: false
+            });
+
+            // 可以进行连线的终点
+            instance.makeTarget(el, {
+                anchor: 'Continuous',
+                maxConnections: -1,
+                allowLoopback: false //禁止回环
+            });
+        }
+
+        $(".node").each(function (i, t) {
+            addGroup(t, $(t).attr("id"));
+        });
+
+        $(".table-column-type-integer").each(function (i, t) {
+            makeEndpoint(t);
+        });
+    })
+})
